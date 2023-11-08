494b9eb4f093eb143144df78123a919f9d8c3652
Hayri Cicek <hayri.cicek@hiq.se>
2023-11-08 11:24:58 +0100

    Adds get all contacts endpoint

--- src/controllers/crmController.js
+++ src/controllers/crmController.js
@@ -10,3 +10,9 @@ export const addNewContact = async (req, res) => {
     .then((contact) => res.json(contact))
     .catch((err) => err);
 };
+
+export const getContacts = (req, res) => {
+  Contact.find({})
+    .then((contacts) => res.json(contacts))
+    .catch((err) => err);
+};
